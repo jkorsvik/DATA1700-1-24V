62573982904bb0769857f66e2d2593c716b72490
Jon-Mikkel Korsvik <48263282+jkorsvik@users.noreply.github.com>
2024-04-25 19:00:25 +0000

    needed a refresh, simplifying the scope

--- oblig3/src/main/unused_code/Ticket.java
+++ oblig3/src/main/unused_code/Ticket.java
@@ -1,14 +1,8 @@
 package com.example.oblig1;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
-@Entity
 public class Ticket {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Integer id;
 
     private String film;
